812c0f59f7c41922739ea918635fd8d0e657c1fb
quy.hoang <quy.hoang@kiaisoft.com.vn>
2024-07-08 14:29:14 +0700

    feat: signup

--- src/controllers/auth/signup.ts
+++ src/controllers/auth/signup.ts
@@ -1,14 +1,20 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { authService } from '@gateway/services/api/auth.service';
 import { AxiosResponse } from 'axios';
 import { StatusCodes } from 'http-status-codes';
+import { BadRequestError } from '@quysterben/jobber-shared';
 
 export class SignUp {
-  public async create(req: Request, res: Response): Promise<void> {
-    const response: AxiosResponse = await authService.singUp(req.body);
-    req.session = {
-      jwt: response.data.token
-    };
-    res.status(StatusCodes.CREATED).json({ message: response.data.message, user: response.data.user });
+  public async create(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const response: AxiosResponse = await authService.singUp(req.body);
+      req.session = {
+        jwt: response.data.token
+      };
+      res.status(StatusCodes.CREATED).json({ message: response.data.message, user: response.data.user });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+      next(new BadRequestError(err.response.data.message, 'SignUp create() method'));
+    }
   }
 }
